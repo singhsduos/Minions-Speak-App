685cde7c81aa1f590f01bf424afb393b088bbc3e
singhsduos <ns9628491678@gmail.com>
2021-09-01 15:35:26 +0530

    voice update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,50 @@
 var apiAlert = "Note: Only 5 time per hour it translate."
-alert(apiAlert);
+// alert(apiAlert);
+
+var speech = window.speechSynthesis;
+var voices = [];
+var voiceList = document.querySelector("#voiceList");
 var inputBox = document.querySelector("#input-box");
-var translateBtn = document.querySelector("#translate-btn");
 var outputBox = document.querySelector("#output-box");
+var translateBtn = document.querySelector("#translate-btn");
+var listenBtn = document.querySelector("listen-btn");
 var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+
+getVoices();
+if (speechSynthesis !== undefined) {
+    speechSynthesis.onvoiceschanged = getVoices;
+}
+
+// click event for listen button
+function listenBtnClickEvent() {
+    var toSpeak = new SpeechSynthesisUtterance(inputBox.value);
+    var selectedVoiceName = voiceList.selectedOptions[7].getAttribute('data-name');
+    voices.forEach((voice) => {
+        if (voice.name === selectedVoiceName) {
+            toSpeak.voice = voice;
+        }
+    });
+    speech.speak(toSpeak);
+}
+
+
+function getVoices() {
+    voices = speech.getVoices();
+    voiceList.innerHTML = '';
+    voices.forEach((voice) => {
+        var listItem = document.createElement('option');
+        listItem.textContent = voice.name;
+        listItem.setAttribute('data-lang', voice.lang);
+        listItem.setAttribute('data-name', voice.name);
+        voiceList.appendChild(listItem);
+    });
+
+    voiceList.selectIndex = 7;
+}
+
+
+
 
 // function to handle error
 function errorHandler(error) {
@@ -32,3 +73,7 @@ function buttonclickEvent() {
 
 // click event for translation button
 translateBtn.addEventListener('click', buttonclickEvent);
+
+// if (listenBtn) {
+    listenBtn.addEventListener('click', listenBtnClickEvent);
+// }
